55b9ed91a1163203b3579d2dc70b1133cb68b132
Ricardo Sessa <ricardosessafilho@gmail.com>
2021-06-09 22:55:43 -0300

    contagem regressiva

---
+++ Contagem_regressiva.js
@@ -0,0 +1,9 @@
+function solucao(numero) {
+    var numero;
+    for (i = numero; i >= 0 ; i--) {
+        console.log(i)
+        if ( i == 0) {
+            console.log ("KABUM")
+        }   
+    }   
+}
